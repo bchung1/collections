9f72b55a66285f6abb7b8351a8a86ac170b7aed3
Brian Chung <bchung8296@gmail.com>
2021-05-13 00:27:51 -0400

    some prisma stuff

---
+++ pages/api/users.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handle(req, res) {
+    const allUsers = await prisma.user.findMany({
+        include: {
+          posts: true,
+          profile: true,
+        },
+      })
+    res.send(allUsers)
+}
